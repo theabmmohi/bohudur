478de9ec239e16d8818831a414dc4724e4494cc7
theabmmohi <theabmmohi@gmail.com>
2026-09-25 07:12:39 +0600

    Init

---
+++ tsdown.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "tsdown"
+import { readFileSync } from "fs"
+import path from "path"
+
+const { compilerOptions } = JSON.parse(readFileSync("./tsconfig.json", "utf-8"))
+const aliases = Object.fromEntries(
+  Object.entries(compilerOptions.paths as Record<string, string[]>).map(([key, [value]]) => [
+    key.replace("/*", ""),
+    path.resolve(value.replace("/*", ""))
+  ])
+)
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  target: "node18",
+  alias: aliases
+})
